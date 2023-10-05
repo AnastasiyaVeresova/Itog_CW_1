3a5ba7721ac982c1ba817caea083b93816bc9fbe
AV <nastulua@yandex.ru>
2023-10-05 20:36:23 +0300

    Добавляем метод для печати

--- Program.cs
+++ Program.cs
@@ -24,3 +24,12 @@ for(int i = 0; i < array1.Length; i++)
 }
 return array2;
 }
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.WriteLine($"{arr[i]}");
+        else Console.WriteLine($"{arr[i]}!");
+    }
+}
