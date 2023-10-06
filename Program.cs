e21af7e6cf50910393923c04880b7cbf1123f706
AV <nastulua@yandex.ru>
2023-10-06 14:25:53 +0300

    Добавлена проверка на существование строк, подходящих по условию

--- Program.cs
+++ Program.cs
@@ -6,23 +6,26 @@
 string[] FiltrStrings(string[] array1)
 {
     int count = 0;
- for(int i = 0; i < array1.Length; i++)
- {
-    if(array1[i].Length <= 3) count++;
- }
-
-string[] array2 = new string[count];
-int index = 0;
-
-for(int i = 0; i < array1.Length; i++)
-{
-    if(array1[i].Length <= 3)
+    for (int i = 0; i < array1.Length; i++)
     {
-        array2[index] = array1[i];
-        index++;
-    }   
-}
-return array2;
+        if (array1[i].Length <= 3) count++;
+    }
+
+    if (count < 1) Console.Write("В массиве нет строк с заланным условием");
+    else Console.WriteLine("Результат собран из строк по три и менее символов: ");
+
+    string[] array2 = new string[count];
+    int index = 0;
+
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[index] = array1[i];
+            index++;
+        }
+    }
+    return array2;
 }
 
 void PrintArray(string[] arr)
@@ -37,8 +40,8 @@ void PrintArray(string[] arr)
 // Console.Write("Введите элементы массива через пробел: ");
 // string[] array1 = Console.ReadLine().Split();
 
-string[] array1 = {"Привет", "Я", "Ты", "Он", "Она", "Мы", "целая", "страна", "Пока", "гуд"};
+string[] array1 = { "Привет", "Я", "Ты", "Он", "Она", "Мы", "целая", "страна", "Пока", "гуд" };
+// string[] array1 = {"Привет", "целая", "страна", "Пока"};
 string[] array2 = FiltrStrings(array1);
 
-Console.WriteLine("Результат собран из строк по три и менее символов: ");
 PrintArray(array2);
